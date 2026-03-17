8419ae802a56120d393380d0f66f8e11199a4d53
Jesse David Francisco <jdfrancisco5@up.edu.ph>
2026-03-17 23:43:11 +0800

    Replace repo with local bettr files

---
+++ backend/server.js
@@ -0,0 +1,108 @@
+import dotenv from "dotenv";
+import express from "express";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use((req, res, next) => {
+    next();
+})
+
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10, 2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`;
+
+        console.log("Database initialized successfully");
+    
+    } catch (error) {
+        console.log("Error initializing DB", error);
+        process.exit(1);
+    }
+};
+
+app.get("/api/transactions/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+        
+        const transactions = await sql`
+            SELECT * FROM transactions 
+            WHERE user_id = ${userId} 
+            ORDER BY created_at DESC
+         `
+
+         res.status(200).json(transactions);
+    } catch (error) {
+        console.log("Error Fetching the transaction", error);
+        res.status(500).json({ mesage: "Internal server error"});
+    }
+});
+
+
+app.post("/api/transactions", async (req, res) => {
+    try {
+        const { title, amount, category, user_id }  = req.body
+
+        if (!title || !user_id || !category || amount === undefined) {
+            return res.status(400).json({ message: "All fields are required"});
+        }
+
+        const transaction = await sql`
+        INSERT INTO transactions(title,user_id,category,amount)
+        VALUES (${title},${user_id},${category},${amount})
+        RETURNING *
+        `
+        console.log(transaction);
+        res.status(201).json(transaction[0])
+        
+
+    } catch (error) {
+        console.log("Error creating the transaction", error)
+        res.status(500).json({ mesage: "Internal server error"});
+    }
+} )
+
+app.delete("/api/transactions/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (isNaN(parseInt(id))) {
+            return res.status(400).json({message: "Invalid Transaction ID"})
+        }
+        
+        const result = await sql`
+        DELETE FROM transactions
+        WHERE id = ${id}
+        RETURNING *
+        `
+
+        if (result.length === 0) {
+            return res.status(400).json({message: "Transaction not found"});
+        }
+
+        res.status(200).json({ message: "Transaction deleted successfully" });
+
+    } catch (error) {
+        console.log("Error creating the transaction", error)
+        res.status(500).json({ mesage: "Internal server error"});
+    }
+})
+
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is up and running on PORT:", PORT)
+    });
+})
+
